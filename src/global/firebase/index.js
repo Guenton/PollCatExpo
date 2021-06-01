7e9e5ab9035afddc868c088e9e1450bd4b897b55
Guenton <Guenton@Gmail.com>
2021-06-01 06:32:16 -0400

    PCAT-45 Added Firebase connection #promote #comment Added Firebase Connection

---
+++ src/global/firebase/index.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+
+// Optionally import the services that you want to use
+//import "firebase/auth";
+//import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  databaseURL: process.env.FB_DATABASE_URL,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+  measurementId: process.env.FB_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase;
