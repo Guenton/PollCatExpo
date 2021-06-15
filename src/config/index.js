8c375d68d00464bae3d6b9efc6660ca14e093661
Guenton Brooks <guenton.brooks@ibis-management.com>
2021-06-15 11:50:55 -0400

    clear residual firebase apps before initialization

--- src/config/index.js
+++ src/config/index.js
@@ -19,18 +19,17 @@ export const initLanguages = () => {
 };
 
 export const initFirebase = () => {
-  if (!firebase.apps.length) {
-    firebase.initializeApp({
-      apiKey: process.env.FB_API_KEY,
-      authDomain: process.env.FB_AUTH_DOMAIN,
-      databaseURL: process.env.FB_DATABASE_URL,
-      projectId: process.env.FB_PROJECT_ID,
-      storageBucket: process.env.FB_STORAGE_BUCKET,
-      messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-      appId: process.env.FB_APP_ID,
-      measurementId: process.env.FB_MEASUREMENT_ID,
-    });
-  } else {
-    firebase.app();
-  }
+  // Clear residual apps before re-initializing
+  if (firebase.apps.length > 0) firebase.apps.forEach((app) => app.delete());
+
+  firebase.initializeApp({
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    databaseURL: process.env.FB_DATABASE_URL,
+    projectId: process.env.FB_PROJECT_ID,
+    storageBucket: process.env.FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+    appId: process.env.FB_APP_ID,
+    measurementId: process.env.FB_MEASUREMENT_ID,
+  });
 };
